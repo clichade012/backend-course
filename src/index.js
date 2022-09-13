97f909c6c3b7f8c95b4c6ffe8fe0d8b0d03f2e49
Avantica123 <dattalandge001@gmail.com>
2022-09-13 16:37:43 +0530

    setup

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+var bodyParser = require('body-parser');
+
+const route = require('./routes/route')
+const app = express();
+const mongoose=require('mongoose')
+mongoose.connect("mongodb+srv://raj_3028:kWaM507ps0Icsdg0@cluster0.pw23ckf.mongodb.net/Project23", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', route);
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
+
+
+
---
+++ src/routes/route.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router=express.Router()
+// Any Issue Please contact = 9588413799
+
+
+module.exports = router;
+// adding this comment for no reason
