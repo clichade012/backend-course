7668c17d4d0502c32162a4c6535c4ac9c95dfb36
NehaVerma009 <nehuverma009@gmail.com>
2022-09-13 17:28:12 +0530

    InternSchema

---
+++ src/Model.js/InterModel.js
@@ -0,0 +1,40 @@
+const mongoose= require("mongoose")
+
+const internSchema = new mongoose.Schema(
+    {
+        name :{
+            type:String,
+            required :"Name is Required",
+            trim : true
+        },
+
+        email:{
+            type:String,
+            required :"Email is Required",
+            unique :true,
+            trim: true,
+            lowercase:true,
+            match: [/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/, 'Please fill a valid email address']
+
+        },
+        mobile:{
+            type : String,
+            unique : true,
+            required :"Mobile No. is Required",
+            trim :true
+    
+        },
+        collegeId : {type:ObjectId,
+            ref :"collegeModel",
+            trim: true
+        },
+
+        isDeleted:{
+            type : Boolean,
+            default :false
+        }
+
+},{timestamps:true})
+
+
+module.exports = mongoose.model(Intern,internSchema)
