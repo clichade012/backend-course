727e175ce7a41e23f4e9c4f3778a178ec2db9948
NehaVerma009 <nehuverma009@gmail.com>
2022-09-14 12:00:49 +0530

    Logic Completed

--- src/Controllers/CollegeControllers.js
+++ src/Controllers/CollegeControllers.js
@@ -1,5 +1,5 @@
-  const CollegeModel = reqiure('../Models/CollegeModel')
-
+  const CollegeModel = require('../Model/CollegeModel')
+  const InternModel = require('../Model/InternModel')
 
   let checkvalid = function(value){
     if (typeof value == "undefined" || typeof value == "number" || typeof value == null) { return false }
@@ -7,12 +7,47 @@
     return true
   }
 
-  const createcolge = async function(req,res){
+  const createCollege = async function(req,res){
+    try{
       let data = req.body
       
-      let { name , fullName ,logoLink , isDeleted } = data
     let createdata = await CollegeModel.create(data)
-    res.status(201).send({status : true , msg: createdata})
+    res.status(201).send({status : true , data: createdata})
+}
+catch(err){
+  console.log(err.message)
+  res.status(500).send({msg:err.message})
+}
+  }
+
+
+const getCollege = async function (req, res){
+try{
+  let data = req.query
+  let collegeName = data.collegeName
+  if(Object.keys(data).length==0){
+    return res.status(400).send({status :false, message: "Need Some Parameter to get College"  })
+  }
+  let college = await CollegeModel.findOne({name :collegeName})
+  let id = college._id
+  if(!college){
+    return res.status(404).send({status:false,message:"No College Found"})
+  }
+  let Interns = await InternModel.find({collegeId: id})
+  if(!Interns){
+    return res.status(404).send({status: false, message :"Interns Not Found"})
+  }
+  let InternsLength = Interns.length
+
+  res.status(200).send({status: true ,Count : InternsLength,data: college,Interns:Interns})
+}
+catch(err){
+  console.log(err.message)
+  res.status(500).send({msg: err.message})
+}
 }
 
-module.exports=createcolge
+
+
+module.exports.createCollege =createCollege
+module.exports.getCollege =getCollege
