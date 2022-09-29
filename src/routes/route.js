22c86065aedd3fd9a17d3c98afe719ef4510e83b
ashubosh <ashubosh@gmail.com>
2022-09-29 18:08:05 +0530

    WrongApi Debug

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,14 +4,14 @@ const {createUrl,geturl} = require('../controller/urlcontroller')
 
 
 router.post('/url/shorten' , createUrl)
-router.get('/:urlCode' ,geturl)
+router.get('/:urlCode' , geturl)
 
 
+//API for wrong route-Of-API
 router.all("/**", function (req, res) {
     res.status(404).send({
         status: false,
         message: "The api you request is not available"
     })
-
-    
+})
 module.exports = router;
