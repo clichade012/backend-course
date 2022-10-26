0b8d2561f24b5f5d2621814c8e776d8ab9cabdc2
clichade012 <chaitany012@gmail.com>
2022-10-26 17:37:33 +0530

    fgt

--- src/controller/urlcontroller.js
+++ src/controller/urlcontroller.js
@@ -4,7 +4,7 @@ const shortid = require('shortid')
 const redis = require("redis");
 const { promisify } = require("util");
 
-const isValid = function (value){
+const isValid = function (value) {
     if (typeof value === 'undefined' || value === null) return false
     if (typeof value === 'string' && value.trim().length === 0) return false
     if (typeof value === 'number' && value.toString().trim().length === 0) return false
@@ -16,14 +16,14 @@ const redisClient = redis.createClient(
     17456,
     "redis-17456.c301.ap-south-1-1.ec2.cloud.redislabs.com",
     { no_ready_check: true }
-  );
-  redisClient.auth("vZZ1EVOuSk5IAI7ffEGstefqr64pyNiR", function (err) {
+);
+redisClient.auth("vZZ1EVOuSk5IAI7ffEGstefqr64pyNiR", function (err) {
     if (err) throw err;
-  });
-  
-  redisClient.on("connect", async function () {
+});
+
+redisClient.on("connect", async function () {
     console.log("Connected to Redis..");
-  });
+});
 
 
 //Connection setup for redis
@@ -34,20 +34,11 @@ const GET_ASYNC = promisify(redisClient.GET).bind(redisClient)
 
 const createUrl = async function (req, res) {
     try {
-
         const longUrl1 = req.body
 
+        if (Object.keys(longUrl1).length == 0) { return res.status(400).send({ status: false, message: "please input data in body" }) }
 
-
-        if(Object.keys(longUrl1).length == 0){return res.status(400).send({status:false, message: "please input data in body"})}
-
-
-
-
-        const {longUrl} = longUrl1
-
-
-
+        const { longUrl } = longUrl1//destructure
 
         if (!isValid(longUrl)) {
             return res.status(400).send({ status: false, message: "Long URL required" })
@@ -57,48 +48,29 @@ const createUrl = async function (req, res) {
             return res.status(400).send({ status: false, message: "Invalid LongURL" })
         }
 
-       
         if (!longUrl) {
-            
             return res.status(400).send({ status: false, message: "please provide required input field" })
-        
         }
-
-
 
         const baseUrl = "http://localhost:3000"
 
-
-
         if (!validUrl.isUri(baseUrl)) {
-
             return res.status(400).send({ status: false, message: "invalid base URL" })
-
         }
 
-
-
         const cahcedUrlData = await GET_ASYNC(`${longUrl}`)
+
         let short_url = JSON.parse(cahcedUrlData)
-
-
-
-
+         
         if (short_url) {
-
-            return res.status(200).send({ status:true, data: short_url })
-
+            return res.status(200).send({ status: true, data: short_url })
         }
 
         let urlPresent = await UrlModel.findOne({ longUrl: longUrl }).select({ _id: 0, createdAt: 0, updatedAt: 0, __v: 0 })
 
         if (urlPresent) {
-
             await SET_ASYNC(`${longUrl}`, JSON.stringify(urlPresent))
-
-
-            return res.status(200).send({ status: true, message : " Already Present in database" ,data: urlPresent })
-
+            return res.status(200).send({ status: true, message: " Already Present in database", data: urlPresent })
         }
 
         const urlCode = shortid.generate().toLowerCase()
@@ -107,7 +79,7 @@ const createUrl = async function (req, res) {
 
         if (url) {
 
-            return res.status(400).send({ status: false, message: "urlCode already exist in tha db"})
+            return res.status(409).send({ status: false, message: "urlCode already exist in tha db" })
 
         }
 
@@ -147,31 +119,31 @@ const createUrl = async function (req, res) {
 //..............................................get by params.....................................
 
 
-const geturl = async function(req,res){
+const geturl = async function (req, res) {
     let urlc = req.params.urlCode
 
-    if(!urlc){
-        return res.status(400).send({ status: false , message : "Urlcode must be present !"})
+    if (!urlc) {
+        return res.status(400).send({ status: false, message: "Urlcode must be present !" })
     }
 
     let cache = await GET_ASYNC(`${urlc}`)
-    
 
-     cache = JSON.parse(cache)
-    
+
+    cache = JSON.parse(cache)
+
     if (cache) return res.status(302).redirect(cache.longUrl)
 
-    const checkurl = await UrlModel.findOne({ urlCode:urlc})
-      if(!checkurl){
-        return res.status(404).send({ status:false , message: " Url code is not found"})
+    const checkurl = await UrlModel.findOne({ urlCode: urlc })
+    if (!checkurl) {
+        return res.status(404).send({ status: false, message: " Url code is not found" })
     }
 
     await SET_ASYNC(`${urlc}`, JSON.stringify(checkurl))
 
-     return res.redirect(checkurl.longUrl)
-  
+    return res.redirect(checkurl.longUrl)
+
 
 }
 
 
-module.exports= {createUrl,geturl}
+module.exports = { createUrl, geturl }
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const mongoose=require('mongoose');
 const router = require('./routes/route');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+
 
  mongoose.connect("mongodb+srv://rakib123:rakib1234@cluster0.bwxs7wf.mongodb.net/group55Database", {
     useNewUrlParser: true
@@ -23,6 +23,6 @@ app.use(function (req, res) {
     return res.status(404).send({status: false, message: 'Path not found'})
 })
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 3000))
+app.listen( 3000, function () {
+    console.log('Express app running on port ' + ( 3000))
 });
